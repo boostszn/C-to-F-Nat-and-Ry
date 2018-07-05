523f8ced98de47f9edead03873328d9cc6d0c736
Boostszn <31454551+boostszn@users.noreply.github.com>
2018-07-05 13:42:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// https://repl.it/@boostszn1/Natalie-and-Rylan-CF-calculator
+<script>
+function convert(degree) {
+    var x;
+    if (degree == "C") {
+        x = document.getElementById("c").value * 9 / 5 + 32;
+        document.getElementById("f").value = Math.round(x);
+    } else {
+        x = (document.getElementById("f").value -32) * 5 / 9;
+        document.getElementById("c").value = Math.round(x);
+    }
+}
+</script>
